27f5ef60f11927e9f7a43fc4902cd53b3c212ed8
GabbiReis <gabrielastefanereis@gmail.com>
2023-09-23 14:27:09 -0300

    Adicionando arquivos revisão parte 01

---
+++ Revisao_part_1/Revisao1/Controllers/MegaSenaController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Revisao1.Models.Request;
+
+namespace Revisao1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MegaSenaController : ControllerBase
+    {
+
+        private readonly string _megacaminhoArquivo;
+
+        public MegaSenaController()
+        {
+            _megacaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "Data", "MegaSena.json");
+        }
+
+        #region Operaçoes arquivo
+        private List<RegistroJogoViewModel> LerjogosDoArquivo()
+        {
+            if (!System.IO.File.Exists(_megacaminhoArquivo))
+            {
+                return new List<RegistroJogoViewModel>();
+            }
+
+            string json = System.IO.File.ReadAllText(_megacaminhoArquivo);
+            return JsonConvert.DeserializeObject<List<RegistroJogoViewModel>>(json);
+        }
+
+        private void EscreverJogosNoArquivo(List<RegistroJogoViewModel> jogos)
+        {
+            string json = JsonConvert.SerializeObject(jogos);
+            System.IO.File.WriteAllText(_megacaminhoArquivo, json);
+        }
+
+        #endregion
+
+
+        [HttpPost]
+        public IActionResult Post(RegistroJogoViewModel registroJogoViewModel)
+        {
+            
+            var jogosRealizaddos = LerjogosDoArquivo();
+
+            registroJogoViewModel.DataJogo = DateTime.Now;
+
+            if (
+                registroJogoViewModel.PrimeiroNumero != registroJogoViewModel.SegundoNumero
+                && registroJogoViewModel.SegundoNumero != registroJogoViewModel.TerceiroNumero
+                && registroJogoViewModel.TerceiroNumero != registroJogoViewModel.QuartoNumero
+                && registroJogoViewModel.QuartoNumero != registroJogoViewModel.QuintoNumero
+                && registroJogoViewModel.QuintoNumero != registroJogoViewModel.SextoNumero
+               )
+            {
+                jogosRealizaddos.Add(registroJogoViewModel);
+
+                EscreverJogosNoArquivo(jogosRealizaddos);
+
+                return Ok("Jogo registrado com sucesso");
+            }
+            else
+                throw new Exception("O jogo não pode ser registrado pois exite numeros repetidos");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(LerjogosDoArquivo());
+
+        }
+    }
+}
---
+++ Revisao_part_1/Revisao1/Models/Request/RegistroJogoViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Revisao1.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Revisao1.Models.Request
+{
+    public class RegistroJogoViewModel
+    {
+        [Required(ErrorMessage = "O Nome é obrigatória.")]
+        [MinLength(3, ErrorMessage = "O Nome deve ter no mínimo 3 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [CpfValidation(ErrorMessage = "CPF inválido.")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O Número é obrigatório.")]
+        [Range(1, 60, ErrorMessage = "O Número deve ser entre 1 e 60.")]
+        public int PrimeiroNumero { get; set; }
+
+        [Required(ErrorMessage = "O Número é obrigatório.")]
+        [Range(1, 60, ErrorMessage = "O Número deve ser entre 1 e 60.")]
+        public int SegundoNumero { get; set; }
+
+        [Required(ErrorMessage = "O Número é obrigatório.")]
+        [Range(1, 60, ErrorMessage = "O Número deve ser entre 1 e 60.")]
+        public int TerceiroNumero { get; set; }
+
+        [Required(ErrorMessage = "O Número é obrigatório.")]
+        [Range(1, 60, ErrorMessage = "O Número deve ser entre 1 e 60.")]
+        public int QuartoNumero { get; set; }
+
+        [Required(ErrorMessage = "O Número é obrigatório.")]
+        [Range(1, 60, ErrorMessage = "O Número deve ser entre 1 e 60.")]
+        public int QuintoNumero { get; set; }
+
+        [Required(ErrorMessage = "O Número é obrigatório.")]
+        [Range(1, 60, ErrorMessage = "O Número deve ser entre 1 e 60.")]
+        public int SextoNumero { get; set; }
+
+        [JsonIgnore]
+        public DateTime DataJogo { get; set; }
+    }
+}
